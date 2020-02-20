8ce8807669cafcbe1e775833146c9a1839f2946a
dags- <dags@dags.me>
2020-02-20 10:34:45 +0000

    don't place lava in a volcano if it has been carved by a river

--- TerraForgedApp/src/main/java/com/terraforged/app/biome/BiomeProvider.java
+++ TerraForgedApp/src/main/java/com/terraforged/app/biome/BiomeProvider.java
@@ -12,7 +12,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 public class BiomeProvider {
 
