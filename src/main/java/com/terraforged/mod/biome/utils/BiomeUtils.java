c7fac922c99ef26a6d0de8cf7bc16347db39806c
Won-Ton <wonton@terraforged.com>
2020-12-19 18:19:35 +0000

    Reflectively access biome climate first before falling back to codecs

--- src/main/java/com/terraforged/mod/biome/utils/BiomeUtils.java
+++ src/main/java/com/terraforged/mod/biome/utils/BiomeUtils.java
@@ -29,6 +29,8 @@ import com.mojang.serialization.Codec;
 import com.mojang.serialization.Dynamic;
 import com.mojang.serialization.JsonOps;
 import com.terraforged.mod.featuremanager.util.codec.Codecs;
+import com.terraforged.mod.util.reflect.Accessor;
+import com.terraforged.mod.util.reflect.FieldAccessor;
 import net.minecraft.util.RegistryKey;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -40,6 +42,7 @@ public class BiomeUtils {
 
     private static final Codec<Biome.Climate> CLIMATE_CODEC = Biome.Climate.CODEC.codec();
     private static final Map<RegistryKey<Biome>, BiomeBuilder> BUILDERS = new HashMap<>();
+    private static final FieldAccessor<Biome, Biome.Climate> CLIMATE_ACCESSOR = Accessor.field(Biome.class, Biome.Climate.class);
 
     public static BiomeBuilder getBuilder(RegistryKey<Biome> biome) {
         return BUILDERS.computeIfAbsent(biome, BiomeUtils::copy).init();
@@ -74,7 +77,18 @@ public class BiomeUtils {
     }
 
     private static Biome.Climate getClimate(Biome biome) {
-        JsonElement json = Codecs.encodeAndGet(Biome.CODEC, biome, JsonOps.INSTANCE);
-        return Codecs.decodeAndGet(CLIMATE_CODEC, new Dynamic<>(JsonOps.INSTANCE, json));
+        try {
+            return CLIMATE_ACCESSOR.get(biome);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            JsonElement json = Codecs.encodeAndGet(Biome.CODEC, biome, JsonOps.INSTANCE);
+            return Codecs.decodeAndGet(CLIMATE_CODEC, new Dynamic<>(JsonOps.INSTANCE, json));
+        } catch (Throwable t) {
+            t.printStackTrace();
+            return new Biome.Climate(Biome.RainType.RAIN, 0.5F, Biome.TemperatureModifier.NONE, 0.5F);
+        }
     }
 }
