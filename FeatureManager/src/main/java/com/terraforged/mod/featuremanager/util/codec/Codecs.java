df33fd8cd730f28df3026632037ba5699bb689a5
Won-Ton <wonton@terraforged.com>
2021-06-13 15:28:28 +0100

    Remove null checks for Codecs helpers and add extra error logging in #modify

--- FeatureManager/src/main/java/com/terraforged/mod/featuremanager/util/codec/Codecs.java
+++ FeatureManager/src/main/java/com/terraforged/mod/featuremanager/util/codec/Codecs.java
@@ -24,16 +24,10 @@
 
 package com.terraforged.mod.featuremanager.util.codec;
 
-import com.google.common.base.Preconditions;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonNull;
 import com.mojang.datafixers.util.Pair;
-import com.mojang.serialization.Codec;
-import com.mojang.serialization.DataResult;
-import com.mojang.serialization.Dynamic;
-import com.mojang.serialization.DynamicOps;
-import com.mojang.serialization.JsonOps;
-import com.mojang.serialization.OptionalDynamic;
+import com.mojang.serialization.*;
 import com.terraforged.mod.featuremanager.FeatureManager;
 import com.terraforged.mod.util.Environment;
 import org.apache.logging.log4j.Level;
@@ -81,7 +75,6 @@ public class Codecs {
     }
 
     public static <V, T> Optional<V> decode(Codec<V> codec, T input, DynamicOps<T> ops) {
-        Preconditions.checkNotNull(input);
         return getResult(codec.decode(ops, input)).map(Pair::getFirst);
     }
 
@@ -90,22 +83,18 @@ public class Codecs {
     }
 
     public static <V, T> DataResult<T> encode(Codec<V> codec, V value, DynamicOps<T> ops) {
-        Preconditions.checkNotNull(value);
         return codec.encodeStart(ops, value);
     }
 
     public static <V, T> T encodeAndGet(Codec<V> codec, V value, DynamicOps<T> ops) {
-        Preconditions.checkNotNull(value);
         return getOrThrow(codec.encodeStart(ops, value));
     }
 
     public static <V> JsonElement encode(Codec<V> codec, V value) {
-        Preconditions.checkNotNull(value);
         return getResult(encode(codec, value, JsonOps.INSTANCE)).orElse(JsonNull.INSTANCE);
     }
 
     public static <V> Optional<JsonElement> encodeOpt(Codec<V> codec, V value) {
-        Preconditions.checkNotNull(value);
         return getResult(encode(codec, value, JsonOps.INSTANCE));
     }
 
@@ -123,8 +112,13 @@ public class Codecs {
 
     public static <T> T modify(T value, Codec<T> codec, UnaryOperator<JsonElement> modifier) {
         try {
-            JsonElement input = encode(codec, value);
-            JsonElement output = modifier.apply(input);
+            DataResult<JsonElement> input = encode(codec, value, JsonOps.INSTANCE);
+
+            JsonElement output = modifier.apply(input.getOrThrow(true, message -> {
+                CodecException exception = CodecException.of("Failed to encode value %s. Error: %s", value, message);
+                exception.printStackTrace();
+            }));
+
             return decodeAndGet(codec, output, JsonOps.INSTANCE);
         } catch (Throwable t) {
             t.printStackTrace();
